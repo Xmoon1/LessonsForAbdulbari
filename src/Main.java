3a40ed1b46077e227d074f39618f7a7113a3e12e
John Connor <johnconnor04@mail.ru>
2023-08-20 20:47:45 +0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        String a = "Hello";
+//
+//        String b = " world!";
+
+
+        // toLowerCase() - строка в нижний регистр
+        // toUpperCaseCase() - строка в верхний регистр
+//
+//        System.out.println(a.toLowerCase());
+//        System.out.println(a.toUpperCase());
+//        System.out.println(a.isEmpty());
+//        System.out.println(a.equals(b));
+
+
+        // Арефметические операции: + - * / %
+
+//        double number = 20/3f;
+//
+//        System.out.println(20+3);
+//        System.out.println(20-3);
+//        System.out.println(20*3);
+//        System.out.println(number);
+//
+//
+//        String c = a + b;
+//        System.out.println(c);
+
+        Scanner input = new Scanner(System.in);
+
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+
+        System.out.println(a+b+c);
+        System.out.println(a*b*c);
+        System.out.println((a+b+c)/3f);
+
     }
+
 }
